fd5e3ccc0d7ba42c6b20a4f0da4defc3fc6a0ea9
ZHENJIANGTIAN <161424267+ZHENJIANGTIAN@users.noreply.github.com>
2025-09-17 15:06:57 +1000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'smart-bracelet-v1.0.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
